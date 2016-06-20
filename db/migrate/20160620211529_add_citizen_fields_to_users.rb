4100ccb940196f96b8bd22ef28e2499e51db3f33
Abraham Kuri <kurenn@icalialabs.com>
2016-06-20 16:52:13 -0500

    Adds phone fields

--- db/migrate/20160620211529_add_citizen_fields_to_users.rb
+++ db/migrate/20160620211529_add_citizen_fields_to_users.rb
@@ -2,5 +2,9 @@ class AddCitizenFieldsToUsers < ActiveRecord::Migration
   def change
     add_column :users, :name, :string
     add_column :users, :state, :integer
+    add_column :users, :phone_number, :string, unique: true, null: false
+    add_column :users, :phone_confirmation_token, :integer
+
+    add_index :users, :phone_number
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,23 +17,25 @@ ActiveRecord::Schema.define(version: 20160620211529) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email"
+    t.string   "encrypted_password",       default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",            default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
     t.string   "name"
     t.integer  "state"
+    t.string   "phone_number",                          null: false
+    t.integer  "phone_confirmation_token"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["phone_number"], name: "index_users_on_phone_number", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
