c694195045784c7be3ac94f0cbc410d4986e096a
Walter Reyes <wal.reyes.11@gmail.com>
2016-06-21 01:09:18 -0500

    Fixes typo

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -13,7 +13,8 @@ class Vote < ActiveRecord::Base
   private
 
   def vote_unique_per_campaign_per_option
-    if self.class.where(user_id: user_id).joins(:campaign).exists?
+    campaign = option.campaign
+    if self.class.where(user_id: user_id, option: campaign.option_ids).exists?
       errors.add :vote, 'ya votaste'
     end
   end
