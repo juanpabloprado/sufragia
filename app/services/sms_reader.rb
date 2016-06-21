ba85b279e36ed5772b36e3236f9714fa97200584
edolopez <elopez.deleon@gmail.com>
2016-06-21 00:32:52 -0500

    Creates user via SMS

--- app/services/sms_reader.rb
+++ app/services/sms_reader.rb
@@ -12,8 +12,20 @@ class SmsReader
     @text.split(' ')[1].to_i - 1
   end
 
+  def user_zip_code
+    @text.split(' ')[1]
+  end
+
+  def pin_code_assignment
+    @text.split(' ')[2]
+  end
+
   def is_vote_hook?
     @text.split(' ').first.downcase == "/votar"
+  end
+
+  def is_user_creation_hook?
+    @text.split(' ').first.downcase == "/cuenta"
   end
 
 end
--- app/services/sms_slave.rb
+++ app/services/sms_slave.rb
@@ -1,12 +1,15 @@
 class SmsSlave
 
-  def self.create_user(formula, twitter_client, user)
-    #creacion de usuario
+  def self.message_to_new_user(user)
     TEXT_MAGIC_API.send "Tu cuenta en Sufragia ha sido creada satisfactoriamente.", user.phone_with_country_code 
   end
 
   def self.successful_vote(user)
     TEXT_MAGIC_API.send "Tu voto ha sido reflejado en Sufragia.", user.phone_with_country_code 
+  end
+
+  def self.campaign_vote_request(campaign, user)
+  	TEXT_MAGIC_API.send "#{campaign.id} - Pregunta: ", user.phone_with_country_code 
   end
 
 end
